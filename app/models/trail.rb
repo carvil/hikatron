edce7f387d4656d9398b1757e8cdc30361f049dd
Carlos Vilhena <carlosvilhena@gmail.com>
2016-05-17 14:11:09 +0100

    Add validations to the Trail model

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,2 +1,10 @@
 class Trail < ActiveRecord::Base
+  validates :name,
+            :description,
+            :distance,
+            :duration,
+            :local_number,
+            presence: true
+
+  validates :local_number, uniqueness: true
 end
--- spec/models/trail_spec.rb
+++ spec/models/trail_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Trail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:trail) { build(:trail) }
+
+  it 'is a valid trail' do
+    expect(trail).to be_valid
+  end
+
+  describe '#name' do
+    it 'must have a name' do
+      trail.name = nil
+      expect(trail).to_not be_valid
+    end
+  end
+
+  describe '#description' do
+    it 'must have a description' do
+      trail.description = nil
+      expect(trail).to_not be_valid
+    end
+  end
+
+  describe '#distance' do
+    it 'must have a distance' do
+      trail.distance = nil
+      expect(trail).to_not be_valid
+    end
+  end
+
+  describe '#duration' do
+    it 'must have a duration' do
+      trail.duration = nil
+      expect(trail).to_not be_valid
+    end
+  end
+
+  describe '#local_number' do
+    it 'must have a local_number' do
+      trail.local_number = nil
+      expect(trail).to_not be_valid
+    end
+  end
 end
