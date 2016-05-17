ca005d47f0957b15068975d8470c638c9fb820ef
Carlos Vilhena <carlosvilhena@gmail.com>
2016-05-17 15:00:52 +0100

    adding slugs to trails

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,4 +1,7 @@
 class Trail < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :slug_candidates, use: :slugged
+
   validates :name,
             :description,
             :distance,
@@ -7,4 +10,12 @@ class Trail < ActiveRecord::Base
             presence: true
 
   validates :local_number, uniqueness: true
+
+  def slug_candidates
+    [
+      :local_number,
+      [:local_number, :eu_number],
+      [:local_number, :eu_number, :name]
+    ]
+  end
 end
